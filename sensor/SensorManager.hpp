b78318a19d517ae6f0d35554622661d3074c0f4e
MinhTan <minhtannguyentr@gmail.com>
2025-11-03 22:13:25 +0700

    Create SensorManager class

--- sensor/SensorManager.hpp
+++ sensor/SensorManager.hpp
@@ -1,4 +1,26 @@
-#ifndef SENSORMANAGER_HPP_
-#define SENSORMANAGER_HPP_
+#pragma once
+#include <stdint.h>
+#include <array>
 
-#endif
+
+struct SensorSnapshot {
+float temperature; // °C
+float humidity; // %
+float light; // lux (giả lập)
+float pressure; // hPa
+};
+
+
+class SensorManager {
+public:
+SensorManager();
+void begin();
+void update();
+SensorSnapshot getSnapshot() const;
+
+
+private:
+SensorSnapshot snapshot_;
+unsigned long startMillis_;
+float pseudoNoise();
+};
